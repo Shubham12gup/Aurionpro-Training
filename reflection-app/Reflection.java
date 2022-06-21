6683ec9bb5b8af5a09c11ef5f0a853de28e9ab47
Shubham12gup <74044821+Shubham12gup@users.noreply.github.com>
2022-06-21 23:25:58 +0530

    Update Reflection.java

--- reflection-app/Reflection.java
+++ reflection-app/Reflection.java
@@ -5,27 +5,27 @@ import java.lang.reflect.*;
 public class Reflection {
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 	public static final String ANSI_RESET = "\u001B[0m";
-	
+
 	public static void getMethods(Class className) {
 		Method[] methods = className.getDeclaredMethods();
-		System.out.println(ANSI_GREEN_BACKGROUND + "Methods of " +className+ ANSI_RESET);
-		for(Method m : methods) {
+		System.out.println(ANSI_GREEN_BACKGROUND + "Methods of " + className + ANSI_RESET);
+		for (Method m : methods) {
 			String names = m.getName();
 			System.out.println(names);
 		}
 	}
-	
-	public static void getFields(Class className){
+
+	public static void getFields(Class className) {
 		Field[] field = className.getDeclaredFields();
-		System.out.println(ANSI_GREEN_BACKGROUND + "\nFields of " +className+ ANSI_RESET);
-		for(Field f : field) {
+		System.out.println(ANSI_GREEN_BACKGROUND + "\nFields of " + className + ANSI_RESET);
+		for (Field f : field) {
 			System.out.println("Name: " + f.getName());
 			System.out.println("Type: " + f.getType());
 			int mod = f.getModifiers();
-			System.out.println("Modifier: " +Modifier.toString(mod));
+			System.out.println("Modifier: " + Modifier.toString(mod));
 			System.out.println("-------------------");
-			
 		}
+
 	}
 
 }
