823aa986c9f9ab8fb386408d93c725045be8217d
Shubham12gup <74044821+Shubham12gup@users.noreply.github.com>
2022-06-30 20:53:58 +0530

    Update TicTacTest.java

--- Tic-Tac-Toe-Game/TicTacTest.java
+++ Tic-Tac-Toe-Game/TicTacTest.java
@@ -3,8 +3,7 @@ package com.aurionpro.test;
 import java.util.Scanner;
 
 public class TicTacTest {
-	public static final String ANSI_RED = "\u001B[31m";
-	public static final String ANSI_GREEN = "\u001B[32m";
+	
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
